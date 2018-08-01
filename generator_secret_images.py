117982eff7eb7c98037384018627478b53a87ac4
Arthur Provost <arthurprovost@MacBook-Pro-de-Arthur.local>
2018-08-01 22:03:17 +0200

    Font size changed, adding roboto font, and length of secret

--- generator_secret_images.py
+++ generator_secret_images.py
@@ -11,8 +11,10 @@ img_size = 128
 for i in range(nb_image):
 	img = Image.new('RGB', (img_size, img_size), color = (255, 255, 255))
 	positionx = np.random.choice(np.arange(0,img_size-40,1), 1, replace=True)
-	positiony = np.random.choice(np.arange(0,img_size-20,1), 1, replace=True)
-	secret = np.random.choice(list(string.ascii_letters), 6, replace=True)
+	positiony = np.random.choice(np.arange(0,img_size-50,1), 1, replace=True)
+	secret = np.random.choice(list(string.ascii_letters), 1, replace=True)
 	draw = ImageDraw.Draw(img)
-	draw.text((positionx, positiony), ''.join(secret), fill=(0,0,0))
+	font_path = '/Users/arthurprovost/Documents/Steganography/Steganography/Roboto-Black.ttf'
+	font = ImageFont.truetype(font_path, 40)
+	draw.text((positionx, positiony), ''.join(secret), fill=(0,0,0), font=font)
 	img.save('./generated_secret/generated_image%d.png'%i)
